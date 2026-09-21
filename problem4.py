5733fb2d637960a50c179c10b442c710196dfe64
wbrashear01-crypto <wbrashear01@saintmarys.edu>
2026-09-21 16:51:06 -0400

    Fix power_of_three function to return correct values
    Implement power_of_three function logic to handle edge cases.

--- problem4.py
+++ problem4.py
@@ -5,8 +5,14 @@ def power_of_three(n):
   >>> power_of_three(27)
   True
   >>> power_of_three(0)
-  ### what should be the answer here?
+  False
   >>> power_of_three(-10)
   False
+  >>> power_of_three(9)
+  True
   """
-  ## add your code here
+  if n<1:
+    return False
+  while n%3==0:
+    n=n//3
+  return n==1
