a6d9c066665cbb5282fb217daa43dd0b081349ad
wbrashear01-crypto <wbrashear01@saintmarys.edu>
2026-09-25 10:38:16 -0400

    Add Roman to Decimal conversion logic
    Implement logic to convert Roman numerals to decimal.

--- problem3.py
+++ problem3.py
@@ -9,4 +9,27 @@ def roman_to_decimal(string):
   >>> roman_to_decimal("III")
   3
   """
-  ### add your code here
+  while jljkjlk:
+    if  string[0:1]=="CM":
+      n=900+n
+      string=[1:]
+  elif  string[0:2]=="M":
+    n=1000+n
+if string[0:2]=="CD":
+  n=400+n
+elif string[0:2]=="C":
+  n=100+n
+if "XL":
+  n=40+n
+elif "L":
+  n=50+n
+if "IX":
+  n=9+n
+elif "X":
+  n=10+n
+if "IV":
+  n=4+n
+elif "I":
+  n=1+n
+
+
